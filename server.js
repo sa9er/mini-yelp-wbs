4da5db681db1b6696ba29ae79d769008bcffa35f
Saqer <ahmadsaqer95@outloo.com>
2021-05-20 20:24:08 +0200

    asda

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const orders = require('./api/orders');
 const errorHandler = require('./middleware/error');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 connectDB();
 
